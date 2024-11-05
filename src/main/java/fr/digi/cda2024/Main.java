e78b72437145ac8d8e6972fb455c1b648be9d196
popobg <pauline.bouyssou1@gmail.com>
2024-11-05 19:00:42 +0100

    Small fix

--- src/main/java/fr/digi/cda2024/Main.java
+++ src/main/java/fr/digi/cda2024/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotheque-jpa");
         EntityManager em = emf.createEntityManager();
-        em.getTransaction();
+        em.getTransaction().begin();
 
         // Read/Select
         // tp 01
